75253f5304f0a7bb22995d812b689aa58acd683e
derysudrajat <dery.sudrajat17@gmail.com>
2021-07-16 01:14:40 +0700

    change package to io.github.derysudrajat.mathview add icon for math-view

---
+++ mathview/src/main/java/io/github/derysudrajat/mathview/Helpers.kt
@@ -0,0 +1,14 @@
+package io.github.derysudrajat.mathview
+
+import android.app.Activity
+import android.content.res.Configuration
+import java.net.URLEncoder
+
+object Helpers {
+
+    fun encode(url: String?): String = URLEncoder.encode("\\Huge $url", "utf-8")
+
+    fun Activity.isDarkMode(): Boolean =
+        this.resources?.configuration?.uiMode
+            ?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+}
